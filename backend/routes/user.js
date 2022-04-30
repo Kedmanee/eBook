0984bac07d9f134f9ea49db81486acd17877dd3a
praew <63070012@kmitl.ac.th>
2022-05-01 00:28:34 +0700

    init back end

---
+++ backend/routes/user.js
@@ -0,0 +1,31 @@
+const { json } = require("express");
+const express = require("express");
+const pool = require("../config");
+
+const router = express.Router();
+
+// Create user
+router.post('/user/signup', async function (req, res, next) {
+    const conn = await pool.getConnection()
+    // Begin transaction
+    await conn.beginTransaction();
+    const name = req.body.name.split(" ");
+    try {
+        const [rows1, fields1] = await conn.query(
+            'INSERT INTO `customer` (`username`, `password`, `date`, `fname`, `lname`,`phone`,`email`,`grade`,`sex` ) VALUES (?, ?, ?, ?, ?,?,?,"Bronze",?)',
+            [req.body.username, req.body.password,req.body.date, name[0], name[1],req.body.phone,req.body.email,req.body.sex])
+        res.json({
+            message: "success"
+        })
+        await conn.commit()
+
+    } catch (err) {
+        await conn.rollback();
+        return res.status(500).json(err)
+    } finally {
+        console.log('finally')
+        conn.release();
+    }
+});
+
+exports.router = router
