86ab78120f8ce2abdb9811d5fbdb399454345321
praew <63070012@kmitl.ac.th>
2022-05-01 00:21:04 +0700

    init

---
+++ frontend/src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from '@/components/HelloWorld.vue'
+</script>
+
+<template >
+  <div id="app">
+    <!-- nav bar -->
+    <nav
+      class="navbar"
+      role="navigation"
+      aria-label="main navigation"
+      style="background-color: #534340"
+      width="300"
+      height="300"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img
+            src="https://media.discordapp.net/attachments/727925890989817947/969683763044962304/IMG_3599.png?width=670&height=670"
+            style="height: 100%; aspect-ratio: 1/1;"
+          />
+        </a>
+
+        <a
+          role="button"
+          class="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item"> หน้าแรก </a>
+
+          <a class="navbar-item"> มาใหม่ </a>
+
+          <div class="navbar-item has-dropdown is-hoverable">
+            <a class="navbar-link"> e-book </a>
+
+            <div class="navbar-dropdown">
+              <a class="navbar-item"> มังงะ </a>
+              <a class="navbar-item"> นิยาย </a>
+              <a class="navbar-item"> นวนิยาย </a>
+              <hr class="navbar-divider" />
+              <a class="navbar-item"> ทั้งหมด </a>
+            </div>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button" style="background-color:#E9EFC0;" st href="/signUp">
+              Sign up
+              </a>
+              <a class="button" style="background-color:#percent;"> Log in </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </div>
+  <RouterView />
+</template>
+
+<style scoped>
+.navbar-link, a.navbar-item {
+    cursor: pointer;
+    color: #F4FCD9;
+}
+a.navbar-item:hover {
+    background-color: #BB9981;
+    color:  #534340;
+}
+.navbar-link:hover{
+    background-color: #BB9981;
+    color:  #534340;
+}
+.navbar-item.has-dropdown:hover .navbar-link {
+    background-color: #BB9981;
+}
+
+.navbar-dropdown{
+  background-color: #BB9981;
+}
+</style>
---
+++ frontend/src/views/SignUp.vue
@@ -0,0 +1,133 @@
+
+<template>
+  <container>
+    <main class="box content">
+      <div class="container-signUp">
+        <h1>สมัครสมาชิก</h1>
+        <div class="field">
+          <label class="label">ชื่อ - นามสกุล </label>
+          <div class="control">
+            <input class="input" type="text" placeholder="ตัวอย่าง สมชาย มะนาวดอง" v-model="name" />
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">ชื่อผู้ใช้</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input is-success" type="text" placeholder="ตัวอย่าง somchai3310" v-model="username"
+              id="name" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+          <p class="help is-success">ชื่อผู้ใช้สามารถใช้ได้</p>
+        </div>
+        <div class="field">
+          <label class="label">รหัสผ่าน</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" type="password" placeholder="Text input" v-model="password" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-user"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">เบอร์โทรศัพท์</label>
+          <div class="control">
+            <input class="input" type="varchar" placeholder="ตัวอย่าง 098xxxxxxx" id="phone" v-model="phone" />
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">อีเมล</label>
+          <div class="control has-icons-left has-icons-right">
+            <input class="input" type="email" placeholder="ตัวอย่าง somchai1234@gmail.com" v-model="email" />
+            <span class="icon is-small is-left">
+              <i class="fas fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+              <i class="fas fa-exclamation-triangle"></i>
+            </span>
+          </div>
+        </div>
+
+        <div class="field">
+          <label class="label">เพศ</label>
+          <div class="control">
+            <div class="select">
+              <select v-model="sex">
+                <option>ชาย</option>
+                <option>หญิง</option>
+                <option>ไม่ระบุ</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div class="field">
+          <label class="label">วัน/เดือน/ปีเกิด</label>
+          <form>
+            <input type="date" id="birthday" name="birthday" v-model="date" />
+          </form>
+        </div>
+
+        <div class="field">
+          <div class="control"></div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link" @click="submit()">Submit</button>
+          </div>
+          <div class="control">
+            <button class="button is-link is-light">Cancel</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  </container>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      name: "",
+      username: "",
+      password: "",
+      email: "",
+      sex: "",
+      phone: "",
+      date: ""
+    }
+  },
+  methods: {
+    submit() {
+      // Validate all fields
+
+      // เช็คว่าในฟอร์มไม่มี error
+      let data = {
+        name: this.name,
+        username: this.username,
+        password: this.password,
+        email: this.email,
+        sex: this.sex,
+        phone: this.phone,
+        date: this.date
+      };
+      axios
+        .post("http://localhost:5000/user/signup", data)
+        .then((res) => {
+          alert("Sign up Success");
+          this.$router.push("/")
+        })
+        .catch((err) => {
+          alert(err)
+        });
+    },
+  },
+};
+</script>
