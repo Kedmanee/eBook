601b42408ee399c7eb84470869594f014e510559
JungJing <63070061@kmitl.ac.th>
2022-05-06 02:09:13 +0700

    j

--- frontend/src/views/ProfileCustomer.vue
+++ frontend/src/views/ProfileCustomer.vue
@@ -1,29 +1,26 @@
 <template>
   <div>
-      <div class="box3 content">
-    <div class="columns is-centered box3 content">
-      <figure class="image is-128x128">
-        <img
-          class="is-rounded"
-          style="border: 10px solid #c5d8a4"
-          src="http://localhost:5000/static/uploads/avo.jpg"
-        />
-      </figure>
-      
-    </div>
-    <br>
-    <div class="columns is-centered">
-    <h3 >
-          {{user.fname}} {{user.lname}}
-      </h3>
+    <div class="box3 content">
+      <div class="columns is-centered box3 content">
+        <figure class="image is-128x128">
+          <img
+            class="is-rounded"
+            style="border: 10px solid #c5d8a4"
+            src="http://localhost:5000/static/uploads/avo.jpg"
+          />
+        </figure>
       </div>
-      <br>
+      <br />
       <div class="columns is-centered">
-      <h5>
-          Email: {{user.email}} 
-      </h5>
-      <h5>ระดับผู็ใช้งาน: ทองคำ</h5>
-    </div>
+        <h3>{{ user.fname }} {{ user.lname }}</h3>
+      </div>
+      <br />
+      <div class="columns is-centered">
+        <h5>Email: {{ user.email }}</h5>
+      </div>
+      <div class="columns is-centered">
+          <h5 style="color:#FFD700">Rank: GOLD</h5>
+        </div>
     </div>
   </div>
 </template>
